dfa325bdda756037d4266dd02629aabeccfcea59
Daniel-Davis <ddv9461@saintjoe.edu>
2015-02-16 11:12:19 -0600

    Update Fruit.js

--- Fruit.js
+++ Fruit.js
@@ -1,3 +1,6 @@
 // Daniel Davis, Fruit, Array of 5 fruits with 2 different categories//
 
-var array =['apple',]
+var array = ['apple', 'Pear', 'Banana', 'Mango', 'Grape']; // creates an array with 5 different fruits
+
+var color = ''; // holds the value of the color of the fruit
+var weight = 0; // holds the weight of said fruit
